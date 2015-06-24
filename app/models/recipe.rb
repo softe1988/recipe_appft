4b5795752abb7bfd2c3cfae5e2d64c6cc144a35b
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-24 14:51:05 +0000

    added validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ActiveRecord::Base
+validates :name, presence:true, length:{minimum:2}
+validates :category, presence:true, length:{minimum:3}
+validates :instructions, presence:true
+validates :prep_time, presence:true, numericality:{greater_than:0}
+validates :cook_time, presence:true, numericality:{greater_than:0}
 
 def minutes_total
     prep_time + cook_time
